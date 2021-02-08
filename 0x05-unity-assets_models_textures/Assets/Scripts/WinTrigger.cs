f3952e745d8fbcd92b89f3363d32294d0cca0e39
JamesPagani <jaimeagalvezv@gmail.com>
2021-02-07 23:55:00 -0500

    Models and textures project.
    Up to task 10.

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    private Timer timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        // Stopping the timer.
+        timer = other.GetComponent<Timer>();
+
+        timer.enabled = false;
+
+        // Updating the timer color and size.
+        timer.timerText.color = Color.green;
+        timer.timerText.fontSize = 69;
+    }
+}
---
+++ 0x05-unity-assets_models_textures/README.md
@@ -0,0 +1,1 @@
+**Jaime Andrés Gálvez Villamarin**
